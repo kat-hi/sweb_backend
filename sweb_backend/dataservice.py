b12243cbb46ae86a4dc8312124c5cede427b56f4
katharina <katharina.sachs@seerene.com>
2023-02-13 10:42:45 +0700

    fix id mismatch

--- sweb_backend/dataservice.py
+++ sweb_backend/dataservice.py
@@ -23,7 +23,6 @@ def get_valid_image_uri(image_output):
 	checked_files = []
 	from flask import current_app
 	base_download_url = current_app.config.get("IMAGE_BASE_URL")
-	app.logger.info(f'get valid image uri: {base_download_url}')
 	imagelist = eval(image_output[0])
 	app.logger.info(imagelist)
 
@@ -31,15 +30,17 @@ def get_valid_image_uri(image_output):
 		try:
 			regex = 'lnk/[\w]*'
 			image_id = re.search(regex, image['uri']).group().split('lnk/')[1]
-			app.logger.info(f'image id: {image_id}')
-			response = requests.head(base_download_url + image_id)
+			image_uri = f'{base_download_url}{image_id}'
+			app.logger.info(f"check: {image_id}")
+			response = requests.head(image_uri)
+
 			app.logger.info(f"RESPONSE: {response}")
 			if response.status_code is 200 and (
 				response.headers['Content-Type'] == 'image/png' or response.headers['Content-Type'] == 'image/jpeg'):
-				app.logger.info('get_valid_image_uri ' + str(response))
+				app.logger.info('response' + str(response))
 				checked_files.append(base_download_url + image_id)
 			else:
-				app.logger.info('image not available: {}'.format(image))
+				app.logger.info(f'image not available: {image}')
 		except TypeError as e:
-			app.logger.info('TypeError: {}'.format(e))
+			app.logger.info(f'TypeError: {e}')
 	return checked_files
